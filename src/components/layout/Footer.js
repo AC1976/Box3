85029c09610f560e24871b267bdeab401f78ca01
AC1976 <54038836+AC1976@users.noreply.github.com>
2020-01-17 16:27:56 +0100

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,32 +8,20 @@ const Footer = () => (
         <p className="mt-5">Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
       </div>
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Important Links</h2>
+        <h2 className="text-lg font-semibold">Achtergrond</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://codebushi.com">Terms &amp; Conditions</a>
+            <a href="https://uitspraken.rechtspraak.nl/inziendocument?id=ECLI:NL:HR:2019:816">Hoge Raad 14 juni 2019</a>
           </li>
           <li>
-            <a href="https://codebushi.com">Privacy Policy</a>
+            <a href="https://uitspraken.rechtspraak.nl/inziendocument?id=ECLI:NL:GHDHA:2019:3425">Hof Den Haag 18 december 2019</a>
+          </li>
+          <li>
+            <a href="https://zoek.officielebekendmakingen.nl/kst-35026-74.html">Ministerie van Financien</a>
           </li>
         </ul>
       </div>
-      <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Social Media</h2>
-        <ul className="mt-4 leading-loose">
-          <li>
-            <a href="https://dev.to/changoman">Dev.to</a>
-          </li>
-          <li>
-            <a href="https://twitter.com/HuntaroSan">Twitter</a>
-          </li>
-          <li>
-            <a href="https://github.com/codebushi/gatsby-starter-lander">GitHub</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </footer>
+      </footer>
 );
 
 export default Footer;
