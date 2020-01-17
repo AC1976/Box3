478c48eef18bc7125a128dcfd2e8613c9e9f5c40
AC1976 <54038836+AC1976@users.noreply.github.com>
2020-01-17 17:26:02 +0100

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,8 +8,7 @@ const Header = () => (
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
         <div className="w-12 mr-3">
-          <LogoIcon />
-        </div>
+         </div>
         
       </div>
       <div className="flex mt-4 sm:mt-0">
