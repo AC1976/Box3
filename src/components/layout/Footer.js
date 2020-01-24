52f6ad9ca78c19a207e99b85721ca511ce5a1143
AC1976 <54038836+AC1976@users.noreply.github.com>
2020-01-24 10:39:51 +0100

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,22 +7,7 @@ const Footer = () => (
         <h2 className="text-lg font-semibold">Contact</h2>
         <p className="mt-5">Interesse? Want to join the Tea Party? Stuur ons een <a href="mailto:box3teaparty@gmail.com">berichtje</a> en we nemen contact op.</p>
       </div>
-      <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Achtergrond info</h2>
-        <ul className="mt-4 leading-loose">
-          <li>
-            <a href="https://uitspraken.rechtspraak.nl/inziendocument?id=ECLI:NL:HR:2019:816">Hoge Raad 14 juni 2019</a>
-          </li>
-          <li>
-            <a href="https://uitspraken.rechtspraak.nl/inziendocument?id=ECLI:NL:GHDHA:2019:3425">Hof Den Haag 18 december 2019</a>
-          </li>
-          <li>
-            <a href="https://zoek.officielebekendmakingen.nl/kst-35026-74.html">Ministerie van Financien</a>
-          </li>
-        </ul>
-      </div>
-  </div>
-      </footer>
+</footer>
 );
 
 export default Footer;
